ad0694fa3ff2a095d3fe4249ebf2d43c69d8bbb4
il.chudov <Graze74@gmail.com>
2017-11-30 16:12:15 +0300

    Service Bus project added

---
+++ src/main/java/edu/nc/servicebus/ServiceBusApplication.java
@@ -0,0 +1,12 @@
+package edu.nc.servicebus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBusApplication.class, args);
+	}
+}
