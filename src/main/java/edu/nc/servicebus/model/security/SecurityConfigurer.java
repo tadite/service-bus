611957deaa6e60065a2ec66e516b86882ef1bed5
ilchudov <Graze74@gmail.com>
2018-03-15 01:47:01 +0300

    one more fix

--- src/main/java/edu/nc/servicebus/model/security/SecurityConfigurer.java
+++ src/main/java/edu/nc/servicebus/model/security/SecurityConfigurer.java
@@ -5,6 +5,7 @@ import edu.nc.servicebus.model.security.jwt.TokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -46,6 +47,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter{
         return super.authenticationManagerBean();
     }
 
+    @Lazy
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
